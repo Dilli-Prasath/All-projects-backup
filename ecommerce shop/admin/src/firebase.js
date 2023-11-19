04a8eae3e822167e38fca1f28f474977142d3e5f
Dilli Prasath <72815509+Dilli-Prasath@users.noreply.github.com>
2023-11-19 11:42:47 +0530

    Add files via upload

---
+++ ecommerce shop/admin/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDkj0refc3Nw5vCNNrMfgD4qizuvhYofqs",
+  authDomain: "shop-5bc67.firebaseapp.com",
+  projectId: "shop-5bc67",
+  storageBucket: "shop-5bc67.appspot.com",
+  messagingSenderId: "815132521134",
+  appId: "1:815132521134:web:ad8390123f20ee8535333f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
